b4c59679ff8f8e73620d10a705afc4379aec1f7b
Gerasumchuk <78738872+Gerasumchuk@users.noreply.github.com>
2022-10-25 10:20:46 +0300

    Add files via upload

---
+++ Lab01_Herasymchuk/Lab01_Herasymchuk/Form1.Designer.cs
@@ -0,0 +1,167 @@
+﻿namespace Lab01_Herasymchuk
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox_password = new System.Windows.Forms.TextBox();
+            this.textBox_login = new System.Windows.Forms.TextBox();
+            this.button_OK = new System.Windows.Forms.Button();
+            this.label_ok = new System.Windows.Forms.Label();
+            this.OK = new System.Windows.Forms.Label();
+            this.checkPass = new System.Windows.Forms.CheckBox();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.label2);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.textBox_password);
+            this.groupBox1.Controls.Add(this.textBox_login);
+            this.groupBox1.Location = new System.Drawing.Point(42, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(199, 86);
+            this.groupBox1.TabIndex = 0;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Авторизація";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(7, 54);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(56, 16);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "Пароль";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(7, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 16);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Логін";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // textBox_password
+            // 
+            this.textBox_password.Location = new System.Drawing.Point(87, 51);
+            this.textBox_password.Name = "textBox_password";
+            this.textBox_password.Size = new System.Drawing.Size(100, 22);
+            this.textBox_password.TabIndex = 2;
+            this.textBox_password.TextChanged += new System.EventHandler(this.textBox_password_TextChanged);
+            // 
+            // textBox_login
+            // 
+            this.textBox_login.Location = new System.Drawing.Point(87, 21);
+            this.textBox_login.Name = "textBox_login";
+            this.textBox_login.Size = new System.Drawing.Size(100, 22);
+            this.textBox_login.TabIndex = 1;
+            this.textBox_login.TextChanged += new System.EventHandler(this.textBox1_TextChanged);
+            // 
+            // button_OK
+            // 
+            this.button_OK.Location = new System.Drawing.Point(89, 130);
+            this.button_OK.Name = "button_OK";
+            this.button_OK.Size = new System.Drawing.Size(75, 23);
+            this.button_OK.TabIndex = 1;
+            this.button_OK.Text = "Пуск";
+            this.button_OK.UseVisualStyleBackColor = true;
+            this.button_OK.Click += new System.EventHandler(this.button_OK_Click);
+            // 
+            // label_ok
+            // 
+            this.label_ok.AutoSize = true;
+            this.label_ok.BackColor = System.Drawing.Color.Red;
+            this.label_ok.Location = new System.Drawing.Point(185, 111);
+            this.label_ok.Name = "label_ok";
+            this.label_ok.Size = new System.Drawing.Size(0, 16);
+            this.label_ok.TabIndex = 5;
+            // 
+            // OK
+            // 
+            this.OK.AutoSize = true;
+            this.OK.Font = new System.Drawing.Font("Microsoft Sans Serif", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.OK.ForeColor = System.Drawing.SystemColors.ActiveCaptionText;
+            this.OK.Location = new System.Drawing.Point(184, 133);
+            this.OK.Name = "OK";
+            this.OK.Size = new System.Drawing.Size(57, 20);
+            this.OK.TabIndex = 6;
+            this.OK.Text = "Status";
+            this.OK.Click += new System.EventHandler(this.OK_Click);
+            // 
+            // checkPass
+            // 
+            this.checkPass.AutoSize = true;
+            this.checkPass.Location = new System.Drawing.Point(129, 104);
+            this.checkPass.Name = "checkPass";
+            this.checkPass.Size = new System.Drawing.Size(143, 20);
+            this.checkPass.TabIndex = 8;
+            this.checkPass.Text = "Показати пароль";
+            this.checkPass.UseVisualStyleBackColor = true;
+            this.checkPass.CheckedChanged += new System.EventHandler(this.checkBox2_CheckedChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(293, 179);
+            this.Controls.Add(this.checkPass);
+            this.Controls.Add(this.OK);
+            this.Controls.Add(this.label_ok);
+            this.Controls.Add(this.button_OK);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.TextBox textBox_login;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox_password;
+        private System.Windows.Forms.Button button_OK;
+        private System.Windows.Forms.Label label_ok;
+        private System.Windows.Forms.Label OK;
+        private System.Windows.Forms.CheckBox checkPass;
+    }
+}
+
---
+++ Lab01_Herasymchuk/Lab01_Herasymchuk/Form1.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Security;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Security.Cryptography;
+using System.IO;
+
+
+namespace Lab01_Herasymchuk
+{
+    public partial class Form1 : Form
+    {
+        string login = File.ReadAllText(@"c:\UNIVER\RTF\Labs\Lab01_Herasymchuk\Lab01_Herasymchuk\login.txt", Encoding.Default);
+        string password = File.ReadAllText(@"c:\UNIVER\RTF\Labs\Lab01_Herasymchuk\Lab01_Herasymchuk\password.txt", Encoding.Default);
+        public Form1()
+        {
+           // login = "58d7ea82e6716d7d8712695cb2da50d817effe6355bc70553f0d73141169ae40";//GER
+           //password = "5994471abb01112afcc18159f6cc74b4f511b99806da59b3caf5a9c173cacfc5";//12345
+            InitializeComponent();
+          textBox_password.UseSystemPasswordChar = true;
+        }
+        private static string ComputeSha256Hash(string s)
+        {
+            var sha256 = SHA256.Create();
+            byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(s));
+            var result = new StringBuilder();
+            for (int i = 0; i <bytes.Length; i++)
+                result.Append(bytes[i].ToString("x2"));
+            return result.ToString();
+            }
+        private void button_OK_Click(object sender, EventArgs e)
+        {
+            //MessageBox.Show(login);
+            //MessageBox.Show(ComputeSha256Hash(textBox_login.Text));
+            //MessageBox.Show(password);
+            //MessageBox.Show(ComputeSha256Hash(textBox_password.Text));
+            if (ComputeSha256Hash(textBox_login.Text) == login && ComputeSha256Hash(textBox_password.Text) == password)
+            {
+                OK.BackColor = Color.Green;
+                MessageBox.Show("Welcome!");
+            }
+            else
+            {
+                OK.BackColor = Color.Red;
+                MessageBox.Show("Incorrect login or password");
+                       }
+        }
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void OK_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox_password_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+       private void checkBox2_CheckedChanged(object sender, EventArgs e)
+        {
+          if (checkPass.Checked)
+          {
+             textBox_password.UseSystemPasswordChar = false;
+           }
+           else
+           {
+               textBox_password.UseSystemPasswordChar = true;
+           }
+        }
+    }
+}
